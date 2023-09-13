89985354c82d396469ac60ab7ac2f5e00354519c
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2023-09-13 11:57:59 +0100

    fix parliamentary_constituency

--- Civi/Inlay/ClickToTweet.php
+++ Civi/Inlay/ClickToTweet.php
@@ -118,7 +118,7 @@ class ClickToTweet extends InlayType {
     $result += [
       'mpName'         => $mp['display_name'],
       'mpTwitter'      => $this->extractTwitterHandleFromUrl($mp['twitter.url'] ?? ''),
-      'mpConstituency' => $this->extractTwitterHandleFromUrl($request['parliamentary_constituency']),
+      'mpConstituency' => $request['parliamentary_constituency'],
     ];
     if (empty($result['mpTwitter'])) {
       $result += [
@@ -145,6 +145,7 @@ class ClickToTweet extends InlayType {
       switch ($matches[1] ?? '') {
       case 'mpName':
       case 'mpTwitter':
+      case 'mpConstituency':
         $_ = $result[$matches[1]] ?? '';
         break;
 
