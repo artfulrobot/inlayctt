8628f0f9d0799591d685901c3cdeb96a5def49de
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2024-03-05 12:08:50 +0000

    Update inlayctt.php

--- inlayctt.php
+++ inlayctt.php
@@ -35,59 +35,12 @@ function inlayctt_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function inlayctt_civicrm_postInstall() {
-  _inlayctt_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function inlayctt_civicrm_uninstall() {
-  _inlayctt_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function inlayctt_civicrm_enable() {
   _inlayctt_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function inlayctt_civicrm_disable() {
-  _inlayctt_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function inlayctt_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _inlayctt_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function inlayctt_civicrm_entityTypes(&$entityTypes) {
-  _inlayctt_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
