6f30927dea749f27d558b9a994b735cb202bf211
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2024-03-05 12:08:46 +0000

    civix upgrade

--- CRM/Inlayctt/Upgrader.php
+++ CRM/Inlayctt/Upgrader.php
@@ -4,7 +4,7 @@ use CRM_Inlayctt_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Inlayctt_Upgrader extends CRM_Inlayctt_Upgrader_Base {
+class CRM_Inlayctt_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
